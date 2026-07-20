f6644b473d79e3c38460464f6f1edebdd0cf55cf
Mridul123885 <mistrymridul3@gmail.com>
2026-07-20 14:10:45 +0800

    Add initial Capacitor configuration file

---
+++ www/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.mridul.myapp',
+  appName: 'My HTML App',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
